ab1f3d05a26fe6ba7ed230089942272b570a11ad
peter <peterlozano89@gmail.com>
2025-06-18 21:23:17 -0400

    Commit file name change. Commit main.py Commit Lozano Week 1 through 3 files Commit Corrected Weekly files Commit Week 4 assignment Commit refactored code

--- Lozano_Week3_Assignment.py
+++ Lozano_Week3_Assignment.py
@@ -3,7 +3,7 @@
 # Programming Assignment Week 3
 # Author Peter Lozano
 # 06/17/2025
-################################################### Assignment Details #############################################
+# ###################################### Assignment Details #######################################
 # Purpose of program:
 # 1. Print welcome statement for user.
 # 2. Retrieve company name.
@@ -27,7 +27,8 @@ locale.setlocale(locale.LC_ALL, '')
 print(f'Welcome to \'{os.path.basename(__file__)}\'')
 user_name = input("Please tell me your name?\n")
 print(f"Welcome {user_name}! We aim to provide you the best fiber optic "
-      f"cables at the best price guaranteed!")
+      f"cables at the best price guaranteed!"
+)
 
 # Requesting user to provide us the company name for receipt purposes.
 company_name = input("What is the name of your company?\n")
@@ -49,61 +50,52 @@ def get_amount_requested(prompt):
                 ' Please enter valid number for calculation.')
 
 
-number_of_feet = get_amount_requested("To calculate total cost,\n"
-                                      "Please enter the number (in feet) how much "
-                                      "fiber optic cable is needed?\n"
-                                      "Enter your number here: "
-                                      )
+number_of_feet = get_amount_requested(
+    "To calculate total cost,\n"
+    "Please enter the number (in feet) how much "
+    "fiber optic cable is needed?\n"
+    "Enter your number here: "
+)
 
 # Creating variables to calculate the total cost.
 # cost_per_foot = 0.95
-def cost_calculator(amount_requested, cost_per_foot= 0.95):
+def cost_calculator(amount_requested):
     """
-    Provides the total cost per foot of fiber optic cable
-    with the standard/discount applied.
-    :param amount_requested: The amount in feet for fiber optic cable.
-    :param cost_per_foot: The standard {0.95} /
-    discount cost per foot depending on amount requested.
-    :return: total_cost ==> float, cost_per_foot ==> float
+    Calculates total installation cost based on a tiered pricing model.
     """
     try:
-        if amount_requested <= 100:  # First condition
-            # Multiplies based on default cost
-            total_cost = amount_requested * cost_per_foot
-            return total_cost, cost_per_foot
-        elif 100 < amount_requested <= 250:  # Second condition if met
-            # cost_per_foot changes due to amount_requested
-            cost_per_foot = 0.85
-            total_cost = amount_requested * cost_per_foot
-            return total_cost, cost_per_foot
-        elif 250 < amount_requested <= 500:  # Third condition if met
-            cost_per_foot = 0.75
-            total_cost = amount_requested * cost_per_foot
-            return total_cost, cost_per_foot
-        else:  # All other conditions should fall into this condition
+        if amount_requested > 500:
             cost_per_foot = 0.55
-            total_cost = amount_requested * cost_per_foot
-            return total_cost, cost_per_foot
+        elif amount_requested > 250:
+            cost_per_foot = 0.75
+        elif amount_requested > 100:
+            cost_per_foot = 0.85
+        else:
+            cost_per_foot = 0.95
+
+        # Calculating total cost based on met condition
+        total_cost = amount_requested * cost_per_foot
+        return total_cost, cost_per_foot
     # Exception will trigger when a non-numeric value is entered.
     except Exception as e:
         print(f'Exception {e} has occurred. Please enter valid number for calculation.')
         return None
 
 # Initializing the cost_calculator function
-cost_calculator(number_of_feet)
+calculated_cost, final_rate_per_foot = cost_calculator(number_of_feet)
 
 # Receipt is printed for user.
-print("%.2f"% number_of_feet)
 print(f"Thank you {user_name}!\n"
-      "Here is your receipt:\n"
-      f"{'#' * 20} Receipt {'#' * 20}\n"
-      f"Date: {date.today().strftime("%m/%d/%Y")}\n"
+      "Here is your receipt:\n")
+print(f"{'#' * 20} Receipt {'#' * 20}")
+print(f"Date: {date.today().strftime("%m/%d/%Y")}\n"
+      f"Purchaser: {user_name}\n"
       f"Company Name: {company_name}\n"
       "Fiber optic cable ordered: "
       f"{locale.format_string("%.2f", number_of_feet, grouping=True)} feet.\n"
       "Cost per foot: "
-      f"{locale.currency(cost_calculator(number_of_feet)[1], grouping=True)}\n"
+      f"{locale.currency(calculated_cost, grouping=True)}\n"
       "Total Cost: "
-      f"{locale.currency(cost_calculator(number_of_feet)[0], grouping=True)}\n"
-      f"{'#' * 22} End {'#' * 22}\n"
+      f"{locale.currency(final_rate_per_foot, grouping=True)}"
 )
+print(f"{'#' * 22} End {'#' * 22}\n")
