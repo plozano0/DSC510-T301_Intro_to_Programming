3e6ba15c4a4073cced95f43eecccb9215701d603
Peter Lozano <peterlozano89@gmail.com>
2025-06-20 00:51:08 -0400

    Commit file name change. Commit main.py Commit Lozano Week 1 through 3 files Commit Corrected Weekly files Commit Week 4 assignment Commit refactored code

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hello World")
+import numpy as np
+
+x = np.arange(6)
+condlist = [x < 3, x > 3]
+choicelist = [-x, x ** 2]
+result = np.select(condlist, choicelist, default=42)
+print(result)  # Output: [ 0 -1 -2 42 16 25]
